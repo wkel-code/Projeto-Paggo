72e054a4e572c38f4daf0352e5a74049b3fe62df
Raquel Silva <raquel_silvas@hotmail.com>
2024-12-16 16:05:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-  outDir: 'build',
-  },
-});
-     
 function App() {
   const [file, setFile] = useState(null);
   const [fileName, setFileName] = useState('');
